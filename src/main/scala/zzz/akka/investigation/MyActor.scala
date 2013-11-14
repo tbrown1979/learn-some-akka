a086e918b882d5bfbd2b12259c4b59da2ebe92d5
Taylor Brown <taylor.brown@banno.com>
2013-11-13 20:47:38 -0600

    Added MyActor, giving an example of why passing case classes is better than raw strings

---
+++ src/main/scala/zzz/akka/investigation/MyActor.scala
@@ -0,0 +1,18 @@
+package zzz.akka.investigation
+
+import akka.actor.Actor
+
+case class Gamma(g: String)
+case class Beta(b: String, g: Gamma)
+case class Alpha(b1: Beta, b2: Beta)
+
+class MyActor extends Actor {
+  def receive = {
+    case "Hello" => println("Hi")
+    case 42 => println("I don't know the question.Go ask the Earth Mark II.")
+    case s: String => println(s"You sent me a string: $s")
+    case Alpha(Beta(b1, Gamma(g1)), Beta(b2, Gamma(g2))) => println(s"beta1: $b1, beta2: $b2, gamma1: $g1, gamma2: $g2")  
+    case _ => println("Huh?")
+  }
+}
+
